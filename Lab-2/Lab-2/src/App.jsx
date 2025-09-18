8202bccb4aa772454e938663b89d18be3bcc50d4
Latrell <trelly63@gmail.com>
2025-09-18 17:11:51 -0400

    Lab 2 finished

---
+++ Lab-2/Lab-2/src/App.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+// Import images
+import sample1 from "./assets/images/sample1.jpg";
+import sample2 from "./assets/images/sample2.jpg";
+import sample3 from "./assets/images/sample3.jpg";
+import sample4 from "./assets/images/sample4.jpg";
+import sample5 from "./assets/images/sample5.jpg";
+import sample6 from "./assets/images/sample6.jpg";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Resorts Lite</h1>
+
+      <div className="card-container">
+        <Card
+          title="Indonesia"
+          image={sample1}
+          description="Gili Air Hotel"
+          rating={4.8}
+          price="589"
+        />
+
+        <Card
+          title="seychelles"
+          image={sample2}
+          description="Hilton Resort"
+          rating={4.2}
+          price="629"
+        />
+
+        <Card
+          title="US Virgin Islands"
+          image={sample3}
+          description="Goa Resort"
+          rating={3.5}
+          price="485"
+        />
+        <Card
+          title="Bahamas"
+          image={sample4}
+          description="Kuredu Resort"
+          rating={4.1}
+          price="729"
+        />
+        <Card
+          title="Mauritius"
+          image={sample5}
+          description="Trou D'eau Douce"
+          rating={4.9}
+          price="877"
+        />
+        <Card
+          title="Bermuda"
+          image={sample6}
+          description="Staniel Cay Hotel"
+          rating={3.2}
+          price="365"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lab-2/Lab-2/src/components/Card.jsx
@@ -0,0 +1,23 @@
+// src/components/Card.jsx
+import React from "react";
+import "./Card.css";
+
+function Card({ title, image, description, rating, price }) {
+  const ratingClass = rating > 4.0 ? "green" : "red";
+
+  return (
+    <div className="card">
+      <img src={image} alt={title} className="card-img" />
+      <h2 className="card-title">{title}</h2>
+      <p>{description}</p>
+      <p className={`rating ${ratingClass}`}>{rating} ★</p>
+
+      {/* Price anchored at bottom */}
+      <div className="card-footer">
+        <p className="price">{price} / night</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
